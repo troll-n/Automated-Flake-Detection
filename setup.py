b747f644c57d6282a39d03f7f154822b1dd28dc0
rirert2 <pkaczmar@andrew.cmu.edu>
2024-07-18 16:28:41 -0400

    fix repo metadata
    (idk how to format a good repo so i copied 2dmatgmm because it was good oops)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def parse_requirements(file):
 
 
 setup(
-    name="GMMDetector",
+    name="Automated-Flake-Detection",
     version="0.1",
     packages=find_packages(),
     install_requires=parse_requirements("requirements.txt"),
@@ -16,15 +16,13 @@ setup(
             # if there are any scripts included in your package
         ],
     },
-    author="Jan-Lucas Uslu",
-    author_email="janlucas.uslu@gmail.com",
-    description="A package for detecting 2D Material flakes using a Gaussian Mixture Model in images.",
+    author="Patrick Kaczmarek",
+    author_email="pkaczmar@andrew.cmu.edu",
+    description="Main repo for code related to the Automated Flake Detection project under Professor Sufei Shi, CMU Dept. of Physics",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/jaluus/2DMatGMM",  # if exists
+    url="https://github.com/rirert2/Automated-Flake-Detection",  # if exists
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
     ],
