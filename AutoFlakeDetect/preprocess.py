9ab749db42557ef4daf1cc272f9ac8bb041d0500
Patrick Kaczmarek <146888767+troll-n@users.noreply.github.com>
2025-02-05 13:00:43 -0500

    pre-contrast save

--- AutoFlakeDetect/preprocess.py
+++ AutoFlakeDetect/preprocess.py
@@ -18,19 +18,20 @@ def SharpandContrast(arr) :
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 IN_DIR = os.path.join(FILE_DIR, "test") \
 
-jpeg_name = "1.jpg"
+"""jpeg_name = "5.jpg"
 jpeg_path = os.path.join(IN_DIR, jpeg_name)
 
 img = Image.open(jpeg_path)
 sharp = ImageEnhance.Sharpness(img)
 contrast = ImageEnhance.Contrast(img)
-sharp.enhance(1.5).show()
-sharp.enhance(2).show()
-
 """
-for i in range(1,5):
+
+
+for i in range(1,6):
     jpeg_name = "{}.jpg".format(i)
     jpeg_path = os.path.join(IN_DIR, jpeg_name)
-    imgnp = cv2.imread(jpeg_path)
-    SharpandContrast(imgnp)
-    """
+    img = Image.open(jpeg_path)
+    sharp = ImageEnhance.Sharpness(img)
+    contrast = ImageEnhance.Contrast(img)
+    contrast.enhance(2).save(jpeg_path)
+
